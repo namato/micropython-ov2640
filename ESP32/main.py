c11f03f4ae01ba8ebfeb60ab5b83a89bc75c7a9a
Christopher M Johnston <40300884+cj667113@users.noreply.github.com>
2019-09-03 20:57:41 -0500

    Update main.py

--- ESP32/main.py
+++ ESP32/main.py
@@ -2,44 +2,27 @@ import ov2640
 import gc
 import time
 import sys
-import network
-import socket
-import os
-import ubinascii
-import machine
-machine.freq(240000000)
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('TG1672G22','TG1672G0A4022') 
-print('waiting....')
-server_address=("192.168.0.10",int("5005"))
-FNAME = 'image.jpg'
+
+FNAME = 'image2.jpg'
+
 def main():
     try:
+        print("initializing camera")
+        #cam = ov2640.ov2640(resolution=ov2640.OV2640_320x240_JPEG)
+        cam = ov2640.ov2640(resolution=ov2640.OV2640_1024x768_JPEG)
         print(gc.mem_free())
+    
         clen = cam.capture_to_file(FNAME, True)
         print("captured image is %d bytes" % clen)
+        print("image is saved to %s" % FNAME)
+    
+        time.sleep(10)
+        sys.exit(0)
     
     except KeyboardInterrupt:
         print("exiting...")
         sys.exit(0)
-def trx():
-  f = open('image.jpg','rb')
-  data=f.read()
-  f.close()
-  s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) 
-  s.connect(server_address)
-  s.sendall(data)
-  rdata=s.recvfrom(1024)
-  print("Recieved Message")
-  s.close()
+
 
 if __name__ == '__main__':
-  print("initializing camera")
-  cam = ov2640.ov2640(resolution=ov2640.OV2640_320x240_JPEG)
-  #cam = ov2640.ov2640(resolution=ov2640.OV2640_1024x768_JPEG)
-  while True:
-      main()
-      trx()
-
+    main()
